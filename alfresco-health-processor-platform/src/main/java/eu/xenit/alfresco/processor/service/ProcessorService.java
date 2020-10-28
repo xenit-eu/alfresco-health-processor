69706e3a7fe754129c67d28ce06cd68c76e7b313
Zlatin Todorinski <zlatin.todorinski@xenit.eu>
2020-10-28 18:09:18 +0100

    Added bare-bone subsystem

---
+++ alfresco-health-processor-platform/src/main/java/eu/xenit/alfresco/processor/service/ProcessorService.java
@@ -0,0 +1,5 @@
+package eu.xenit.alfresco.processor.service;
+
+public interface ProcessorService {
+    void validateHealth();
+}
---
+++ alfresco-health-processor-platform/src/main/java/eu/xenit/alfresco/processor/service/ProcessorServiceImpl.java
@@ -0,0 +1,7 @@
+package eu.xenit.alfresco.processor.service;
+
+public class ProcessorServiceImpl implements ProcessorService {
+    @Override
+    public void validateHealth() {
+    }
+}
