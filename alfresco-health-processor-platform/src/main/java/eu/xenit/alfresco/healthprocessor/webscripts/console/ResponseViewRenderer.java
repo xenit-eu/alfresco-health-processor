xenit-eu/alfresco-heath-processor

Add fixer plugins that can fix unhealthy nodes

alfresco-health-processor-platform/src/main/java/eu/xenit/alfresco/healthprocessor/webscripts/console/ResponseViewRenderer.java
@@ -1,11 +1,13 @@
 package eu.xenit.alfresco.healthprocessor.webscripts.console;
 
+import eu.xenit.alfresco.healthprocessor.fixer.api.HealthFixerPlugin;
 import eu.xenit.alfresco.healthprocessor.indexing.IndexingConfiguration;
 import eu.xenit.alfresco.healthprocessor.indexing.IndexingStrategy;
 import eu.xenit.alfresco.healthprocessor.plugins.api.HealthProcessorPlugin;
 import eu.xenit.alfresco.healthprocessor.processing.ProcessorService;
 import eu.xenit.alfresco.healthprocessor.reporter.api.HealthReporter;
 import eu.xenit.alfresco.healthprocessor.webscripts.console.model.AdminConsoleResponseView;
+import eu.xenit.alfresco.healthprocessor.webscripts.console.model.FixersView;
 import eu.xenit.alfresco.healthprocessor.webscripts.console.model.IndexingStrategyView;
 import eu.xenit.alfresco.healthprocessor.webscripts.console.model.PluginsView;
 import eu.xenit.alfresco.healthprocessor.webscripts.console.model.ReportersView;
@@ -25,6 +27,7 @@ public class ResponseViewRenderer {
     private IndexingStrategy indexingStrategy;
     private List<HealthProcessorPlugin> plugins;
     private List<HealthReporter> reporters;
+    private List<HealthFixerPlugin> fixers;
 
     @SuppressWarnings("unused")
     public void setServiceRegistry(ServiceRegistry serviceRegistry) {
@@ -37,7 +40,8 @@ public class ResponseViewRenderer {
                 processorService.getState().toString(),
                 new IndexingStrategyView(indexingConfiguration, indexingStrategy),
                 new PluginsView(plugins),
-                new ReportersView(reporters)
+                new ReportersView(reporters),
+                new FixersView(fixers)
         );
     }
 

alfresco-health-processor-platform/src/main/java/eu/xenit/alfresco/healthprocessor/webscripts/console/model/AdminConsoleResponseView.java
@@ -14,10 +14,11 @@ public class AdminConsoleResponseView {
     IndexingStrategyView indexing;
     PluginsView plugins;
     ReportersView reporters;
+    FixersView fixers;
 
     public AdminConsoleResponseView(ModuleDetails moduleDetails, String status, IndexingStrategyView indexing,
-            PluginsView plugins, ReportersView reporters) {
-        this(extractVersionNumber(moduleDetails), status, indexing, plugins, reporters);
+            PluginsView plugins, ReportersView reporters, FixersView fixers) {
+        this(extractVersionNumber(moduleDetails), status, indexing, plugins, reporters, fixers);
     }
 
     private static String extractVersionNumber(ModuleDetails moduleDetails) {

xenit-eu/alfresco-health-processor

alfresco-health-processor-platform/src/main/java/eu/xenit/alfresco/healthprocessor/webscripts/console/ResponseViewRenderer.java
@@ -1,5 +1,6 @@
 package eu.xenit.alfresco.healthprocessor.webscripts.console;
 
+import eu.xenit.alfresco.healthprocessor.fixer.api.HealthFixerPlugin;
 import eu.xenit.alfresco.healthprocessor.indexing.IndexingConfiguration;
 import eu.xenit.alfresco.healthprocessor.indexing.IndexingStrategy;
 import eu.xenit.alfresco.healthprocessor.plugins.api.HealthProcessorPlugin;
@@ -24,6 +25,7 @@ public class ResponseViewRenderer {
     private IndexingStrategy indexingStrategy;
     private List<HealthProcessorPlugin> plugins;
     private List<HealthReporter> reporters;
+    private List<HealthFixerPlugin> fixers;
 
     @SuppressWarnings("unused")
     public void setServiceRegistry(ServiceRegistry serviceRegistry) {
@@ -36,7 +38,8 @@ public class ResponseViewRenderer {
                 processorService.getState().toString(),
                 new IndexingStrategyView(indexingConfiguration, indexingStrategy),
                 new ExtensionsView(plugins),
-                new ExtensionsView(reporters)
+                new ExtensionsView(reporters),
+                new ExtensionsView(fixers)
         );
     }
 

alfresco-health-processor-platform/src/main/java/eu/xenit/alfresco/healthprocessor/webscripts/console/model/AdminConsoleResponseView.java
@@ -14,10 +14,11 @@ public class AdminConsoleResponseView {
     IndexingStrategyView indexing;
     ExtensionsView plugins;
     ExtensionsView reporters;
+    ExtensionsView fixers;
 
     public AdminConsoleResponseView(ModuleDetails moduleDetails, String status, IndexingStrategyView indexing,
-            ExtensionsView plugins, ExtensionsView reporters) {
-        this(extractVersionNumber(moduleDetails), status, indexing, plugins, reporters);
+            ExtensionsView plugins, ExtensionsView reporters, ExtensionsView fixers) {
+        this(extractVersionNumber(moduleDetails), status, indexing, plugins, reporters, fixers);
     }
 
     private static String extractVersionNumber(ModuleDetails moduleDetails) {