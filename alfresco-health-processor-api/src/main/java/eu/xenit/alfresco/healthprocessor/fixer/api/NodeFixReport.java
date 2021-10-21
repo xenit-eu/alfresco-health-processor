01d5bdc7c22bb0b324cf142d4d16e1d7264d1e7f
Lars Vierbergen <lars.vierbergen@xenit.eu>
2021-10-21 12:14:44 +0200

    Document unused NodeRef field from NodeFixReport
    This field is necessary when multiple NodeFixReports are put in a Set,
    because we need something to disambiguate the reports, so they are not
    collapsed into one report.

--- alfresco-health-processor-api/src/main/java/eu/xenit/alfresco/healthprocessor/fixer/api/NodeFixReport.java
+++ alfresco-health-processor-api/src/main/java/eu/xenit/alfresco/healthprocessor/fixer/api/NodeFixReport.java
@@ -1,6 +1,7 @@
 package eu.xenit.alfresco.healthprocessor.fixer.api;
 
 import eu.xenit.alfresco.healthprocessor.extensibility.annotations.ExtensionType;
+import eu.xenit.alfresco.healthprocessor.extensibility.annotations.InternalUseOnly;
 import eu.xenit.alfresco.healthprocessor.extensibility.annotations.OnlyForUseIn;
 import eu.xenit.alfresco.healthprocessor.reporter.api.NodeHealthReport;
 import eu.xenit.alfresco.healthprocessor.reporter.api.NodeHealthReport.PersistableData;
@@ -8,13 +9,13 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import lombok.Value;
 import org.alfresco.service.cmr.repository.NodeRef;
 
@@ -39,10 +40,14 @@ public class NodeFixReport implements PersistableData {
     /**
      * Health report of the attempted fix
      *
-     * @implNote This field is transient so it is not serialized, and thus is not available in {@link
-     * eu.xenit.alfresco.healthprocessor.reporter.api.HealthReporter#onCycleDone(List)}.
+     * @implNote This field is transient, so it is not serialized, to avoid a circular reference.
      *
-     * Since fix reports are already linked via {@link NodeHealthReport#data()}, we can do without this backlink.
+     * This means this link will not be available after the HealthReportsStore has stored a {@link NodeHealthReport}. It
+     * is only used to be able to link a {@link NodeFixReport} returned from {@link HealthFixerPlugin#fix(Class, Set)}
+     * to its corresponding {@link NodeHealthReport} in HealthProcessor internals.
+     *
+     * The HealthProcessor fixers component will link it via {@link NodeHealthReport#data()}, so we have no need for
+     * this backlink afterwards.
      */
     @Getter(onMethod_ = {@OnlyForUseIn(ExtensionType.FIXER)})
     transient NodeHealthReport healthReport;
@@ -51,6 +56,18 @@ public class NodeFixReport implements PersistableData {
      * Messages about this fix
      */
     Set<String> messages;
+
+    /**
+     * Internal, persistent reference to the node of the {@link #healthReport}.
+     *
+     * @implNote This field is here so the serialized {@link NodeFixReport} has a reference to the node it is about.
+     * This is necessary for when {@link NodeFixReport}s are placed in a set (as in the return value of {@link
+     * HealthFixerPlugin#fix(Class, Set)}), because the transient {@link #healthReport} field is not part of {@link
+     * #hashCode()} and {@link #equals(Object)} calculations.
+     */
+    @Getter(AccessLevel.NONE)
+    @ToString.Exclude
+    @InternalUseOnly
     NodeRef nodeRef;
 
     @OnlyForUseIn(ExtensionType.FIXER)
