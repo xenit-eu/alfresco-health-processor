5d3baa4da0f5fb8cdafbdb3c2fd30bb6852e3b45
Pieter-Jan Vrielynck <43749206+pvriel@users.noreply.github.com>
2025-02-06 09:52:53 +0100

    [NYS2AWS-134] added another test

---
+++ alfresco-health-processor-platform/src/test/java/eu/xenit/alfresco/healthprocessor/indexing/threshold/ThresholdIndexingStrategyStateTest.java
@@ -0,0 +1,23 @@
+package eu.xenit.alfresco.healthprocessor.indexing.threshold;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThresholdIndexingStrategyStateTest {
+
+    @Test
+    void getMapRepresentation() {
+        ThresholdIndexingStrategyState state = new ThresholdIndexingStrategyState(1, 2, 3);
+        for (int i = 0; i < 4; i ++) state.getTransactionBatchesQueueSize().incrementAndGet();
+        Map<String, String> expectedRepresentation = Map.of(
+                "current-transaction-id", "1",
+                "max-transaction-id", "2",
+                "running-transaction-mergers", "3",
+                "transaction-batches-queue-size", "4"
+        );
+        assertEquals(expectedRepresentation, state.getMapRepresentation());
+    }
+}
